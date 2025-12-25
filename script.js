343ee800ad0fd50a94f7705acdd7364e26371877
Mouhamed Mbaye <mbaye.mouhamed020@gmail.com>
2025-12-25 17:29:29 +0000

    Ajout de lune

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function createEmoji(x, y) {
   const emoji = document.createElement("span");
   emoji.className = "emoji-pop";
 
-  const emojis = ["💥", "🎈", "˗ˏˋ 𝟐𝟎𝟐𝟔 ˎˊ˗", "ミ★ ★ ★彡"];
+  const emojis = ["💥", "🎈", "˗ˏˋ 𝟐𝟎𝟐𝟔 ˎˊ˗", "✩₊˚.⋆☾⋆⁺₊✧"];
   emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
   emoji.style.left = x + "px";
   emoji.style.top = y + "px";
